a993897f5a5017a55098261feb95fcb56edf123f
joaoamarald <joaopedroamaral6565@gmail.com>
2025-03-04 19:20:39 -0300

    finalizado

---
+++ tabuada/script.js
@@ -0,0 +1,22 @@
+function exibirTabuada() {
+
+    const numero = document.getElementById("numero").value;
+    const resultadoDiv = document.getElementById("resultado");
+  
+    resultadoDiv.innerHTML = '';
+  
+    if (isNaN(numero) || numero === '') {
+        alert("Por favor, informe um número válido.");
+    } else {
+  
+        const num = parseInt(numero);
+  
+  
+        let resultadoHTML = `<h3>Tabuada de ${num}:</h3><ul>`;
+        for (let i = 1; i <= 10; i++) {
+            resultadoHTML += `<li>${num} x ${i} = ${num * i}</li>`;
+        }
+        resultadoHTML += '</ul>';
+        resultadoDiv.innerHTML = resultadoHTML;
+    }
+  }
